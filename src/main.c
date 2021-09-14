3b282ccbc4bc837ebf979c269f7a7c17d24aa471
dhzdhd <ragas5432100@gmail.com>
2021-09-14 19:20:57 +0530

    Added beautification and pausing functionality.

--- src/main.c
+++ src/main.c
@@ -1,9 +1,10 @@
 #include "raylib.h"
 #include "raygui.h"
+#include "stdio.h"
 
 static const int SCREEN_HEIGHT = 1080;
 static const int SCREEN_WIDTH = 1920;
-static const int BALL_RADIUS = 25;
+static const int BALL_RADIUS = 20;
 
 typedef struct Ball
 {
@@ -13,13 +14,12 @@ typedef struct Ball
 } Ball;
 
 // Function prototypes
-void constraintCheck(Vector2 *, float *, float *);
-Vector2 initBallPos(void);
 Vector2 initBallVel(void);
-void handleWallCollison(Ball *);
+void handleWallCollision(Ball *ball);
+void handleBallCollision(Ball *firstBall, Ball *secondBall);
 
-const int COLOR_AMOUNT = 9;
-const Color COLOR_LIST[] = {GREEN, RED, YELLOW, PURPLE, BLUE, DARKBLUE, DARKGREEN, DARKPURPLE, ORANGE};
+const int COLOR_AMOUNT = 10;
+const Color COLOR_LIST[] = {GREEN, RED, YELLOW, PURPLE, BLUE, DARKBLUE, DARKGREEN, DARKPURPLE, ORANGE, LIME, VIOLET};
 
 int ballAmount = 0;
 Ball ballList[1000] = {};
@@ -34,20 +34,20 @@ int main(void)
 
     SetTargetFPS(60);
 
-    bool flag = false;
+    bool unPaused = false;
 
     while (!WindowShouldClose())
     {
         // Update
         if (IsKeyPressed(KEY_SPACE))
         {
-            if (flag == true)
+            if (unPaused == true)
             {
-                flag = false;
+                unPaused = false;
             }
             else
             {
-                flag = true;
+                unPaused = true;
             }
         }
 
@@ -68,7 +68,7 @@ int main(void)
             ballAmount = 0;
         }
 
-        if (flag == true)
+        if (unPaused == true)
         {
             for (int i = 0; i < ballAmount; i++)
             {
@@ -83,10 +83,15 @@ int main(void)
         // Draw
         BeginDrawing();
 
-        ClearBackground(BLACK);
+        ClearBackground(GRAY);
         BeginMode2D(camera);
-
+        DrawGrid(100, 50);
         DrawFPS(0, 0);
+
+        if (!unPaused)
+        {
+            DrawText("PAUSED", SCREEN_WIDTH - 135, 0, 30, GREEN);
+        }
 
         if (ballAmount != 0)
         {
@@ -94,6 +99,21 @@ int main(void)
             {
                 Ball ball = ballList[i];
                 DrawCircle(ball.position.x, ball.position.y, BALL_RADIUS, ball.color);
+
+                if (!unPaused)
+                {
+                    float x = ball.position.x;
+                    float y = ball.position.y;
+                    Vector2 mousePos = GetMousePosition();
+
+                    // If mouse is centered on the ball
+                    if ((x - BALL_RADIUS < mousePos.x && x + BALL_RADIUS > mousePos.x) && (y - BALL_RADIUS < mousePos.y && y + BALL_RADIUS > mousePos.y))
+                    {
+                        char buffer[100];
+                        sprintf(buffer, "%i, %i", (int)ball.position.x, (int)ball.position.y);
+                        DrawText(buffer, ball.position.x - 40, ball.position.y + 30, 20, ball.color);
+                    }
+                }
             }
         }
 
@@ -102,14 +122,6 @@ int main(void)
 
     CloseWindow();
     return 0;
-}
-
-Vector2 initBallPos(void)
-{
-    return (Vector2){
-        GetRandomValue(0 + BALL_RADIUS, SCREEN_WIDTH - BALL_RADIUS),
-        GetRandomValue(0 + BALL_RADIUS, SCREEN_HEIGHT - BALL_RADIUS),
-    };
 }
 
 Vector2 initBallVel(void)
@@ -122,20 +134,22 @@ Vector2 initBallVel(void)
 
 void handleWallCollision(Ball *ball)
 {
-    if (ball->position.x + BALL_RADIUS > SCREEN_WIDTH)
+    if (ball->position.x + BALL_RADIUS > SCREEN_WIDTH || ball->position.x - BALL_RADIUS < 0)
     {
         ball->velocity.x *= -1;
     }
-    if (ball->position.x - BALL_RADIUS < 0)
-    {
-        ball->velocity.x *= -1;
-    }
-    if (ball->position.y + BALL_RADIUS > SCREEN_HEIGHT)
+    if (ball->position.y + BALL_RADIUS > SCREEN_HEIGHT || ball->position.y - BALL_RADIUS < 0)
     {
         ball->velocity.y *= -1;
     }
-    if (ball->position.y - BALL_RADIUS < 0)
+}
+
+void handleBallCollision(Ball *firstBall, Ball *secondBall)
+{
+    bool collision = CheckCollisionCircles(firstBall->position, BALL_RADIUS, secondBall->position, BALL_RADIUS);
+
+    if (collision)
     {
-        ball->velocity.y *= -1;
+        firstBall->velocity.x = 0;
     }
 }
