84f99a29c21b1c30d8cf06c6a3219b417247e4c9
dhzdhd <ragas5432100@gmail.com>
2021-12-30 17:51:18 +0530

    Correct button font size and style

--- src/gui.c
+++ src/gui.c
@@ -8,12 +8,15 @@ void gui_setup(bool *start)
     BeginDrawing();
 
     ClearBackground(BLACK);
+    GuiSetStyle(DEFAULT, 1, 2);
+    GuiSetFont(GuiGetFont());
 
     DrawText("Balls!", SCREEN_WIDTH / 2 - 95, SCREEN_HEIGHT / 2 - 340, 70, GREEN);
-     if (GuiButton((Rectangle){SCREEN_WIDTH / 2 - 150, SCREEN_HEIGHT / 2 - 50, 300, 100}, "Hello"))
+     if (GuiButton((Rectangle){SCREEN_WIDTH / 2 - 150, SCREEN_HEIGHT / 2 - 50, 300, 100}, ""))
      {
          *start = true;
      }
+     DrawText("Enter", SCREEN_WIDTH / 2 - 40, SCREEN_HEIGHT / 2 - 10, 30, GREEN);
 
     EndDrawing();
 }
